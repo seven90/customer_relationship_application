4da6cc21ba20cdd8a05a3893a086fecaed5bcba0
Seven90 <stevesurya@gmail.com>
2014-06-06 12:00:06 -0400

    During the class CRM

--- crm.rb
+++ crm.rb
@@ -1,36 +1,85 @@
-require_relative "contact"
-require_relative "rolodex"
+# require_relative "contact"
+# require_relative "rolodex"
 
 class CRM
+
+	attr_reader :name
+	
 	def initialize(name)
 		@name = name
+		@rolodex = Rolodex.new
+		puts "Welcome to #{name}"
 	end
 
 	def print_main_menu
-		puts "[Add] Add a new contact"
-		puts "[Modify] Modify an existing contact"
-		puts "[Display All] Display all the contacts"
-		puts "[Display Contact] Display one specific contact"
-		puts "[Display Attribute] Display an attribute"
-		puts "[Delete] Delete a contact"
-		puts "[Exit] Exit"
-		puts "Enter a command:"
+		puts "[1] Add a new contact"
+		puts "[2] Modify an existing contact"
+		puts "[3] Delete a contact"
+		puts "[4] Display all the contacts"
+		puts "[5] Display an attribute"
+		puts "[6] Exit"
+		puts "Enter a number:"
 	end
 
 	def main_menu
 		print_main_menu
-		user_selected = gets.downcase
+		user_selected = gets.to_i
 		call_option(user_selected)
 	end
 
 	def call_option(user_selected)
-		add_new_contact if user_selected == 1
-		modify_existing_contact if user_selected == 2
-		delete_a_contact if user_selected == 3
-		display_all_contacts if user_selected == 4
-		display_an_attribute if user_selected == 5
-		exit if user_selected == 6
+		case user_selected
+		when 1 then add_new_contact
+		when 2 then modify_existing_contact
+		when 3 then delete_a_contact
+		when 4 then display_all_contacts
+		when 5 then display_an_attribute
+		when 6 
+			puts "Goodbye"
+			return
+		else
+			puts "Invalid option. Please try again"
+			main_menu
+		end
 	end
+
+	def add_new_contact
+		print "Enter First Name: "
+		first_name = gets.chomp
+		print "Enter Last Name:"
+		last_name = gets.chomp
+		print "Enter Email Address"
+		email = gets.chomp
+		print "Enter a Note: "
+		note = gets.chomp
+		@rolodex.add_contact(Contact.new(first_name, last_name, email, note)) 
+		
+		main_menu 
+	end
+
+class Contact
+	attr_accessor :id, :first_name, :last_name, :email, :note
+
+	def initialize(first_name, last_name, email, note)
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@note = note
+	end
+end
+
+class Rolodex
+	def initialize
+		@contacts = []
+		@contact_id = 1000
+	end
+
+	def add_contact(contact)
+		@contacts << contact
+		contact.id = @contact_id
+		@contact_id +=1
+	end	
+end	
 
 	def self.run(name)
 		crm = CRM.new(name)
@@ -38,4 +87,4 @@ class CRM
 	end
 end
 
-CRM.run("my CRM") 
+CRM.run("My CRM") 
